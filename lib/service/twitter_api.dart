4bc49b471a100391da92a27ac438a2be06a883a8
tobi187 <fischerstobi1998@gmail.com>
2021-10-27 19:22:03 +0200

    jj

--- lib/service/twitter_api.dart
+++ lib/service/twitter_api.dart
@@ -3,26 +3,20 @@ import 'package:http/http.dart' as http;
 class TwitterWorker {
   final urlEndpoint =
       "https://api.twitter.com/1.1/statuses/user_timeline.json?screen_name=";
-  final bearer =
-      "AAAAAAAAAAAAAAAAAAAAAOuAVAEAAAAAZo31h2Cjo7LwHnSTW8UYrKd%2BRDg%3DQvPMW0L4ZNsiBjP16HdKED8e1ZwPmOQ4E9hrea7GQBPNxOcMTJ";
+  final String _apiKey = "7ce9a763-d1b8-4473-82c5-ca51f776a1e8";
 
-  final apiKey = "1ih4mckyg2IEJkx7HR2JBQGcL";
-  final apiSecret = "oWWRFGP1E29ZPYh1FEXoMgm2yM3odchw1PHcK6ivDbtfTM2PKW";
-  final token = "3333051718-OKHGj7KFTIo3a50uQ0879wb1wvQJ3EOcUkX1GXM";
-  final tokenSecret = "wyuUFwb6vjtho1AdgRfUXVGvMbNHXdlntIfgwZPG6mHY4";
   Future<String> getUserTweets(String userName) async {
     try {
       Uri uri = Uri.parse(
-          "https://api.twitter.com/1.1/statuses/user_timeline.json?screen_name=" +
-              userName +
-              "&count=2");
-      final _header = {"Authorization": "Bearer $bearer"};
-      var response = await http.read(uri, headers: _header);
-      if (response.isNotEmpty) {
-        var data = response;
+          "https://sharedshopping.eu.pythonanywhere.com/api/v2/twitter?apikey=" +
+              _apiKey);
+      http.Response res = await http.post(uri, body: {"username": userName});
+
+      if (res.statusCode == 200) {
+        var data = res.body;
         return data;
       } else {
-        return "Error: Status Code";
+        return "Error: Status Code ${res.statusCode}";
       }
     } catch (e) {
       var response = "Error: Sry something went wrong: $e";
